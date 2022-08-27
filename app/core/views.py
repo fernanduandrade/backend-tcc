10abf14bf0376031560dd61f67d2ab975a4b71a6
fernanduandrade <onanduandrade@gmail.com>
2022-08-27 14:10:37 -0300

    fix? parametro search

--- app/core/views.py
+++ app/core/views.py
@@ -1,17 +1,16 @@
 from .models import Place, Category
 from .serializers import PlaceSerializer, CategorySerializer
 from rest_framework import viewsets, filters
-from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
-    filterset_fields = ['title']
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    filterset_fields = ['category_name']
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['category_name']
