a9fa979456e9ac084c20aab8be70b59e69b2b8d6
fernanduandrade <onanduandrade@gmail.com>
2022-08-27 00:34:13 -0300

    add: adicionado primeiras entidades da api

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category
+from .models import Place
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Place)
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	category_name = models.CharField(max_length=100)
+	def __str__(self) -> str:
+			return self.category_name
+	class Meta:
+		verbose_name_plural = 'Category'
+
+
+class Place(models.Model):
+    """Place object"""
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    contact = models.CharField(max_length=50)
+    description = models.TextField()
+    img_links = models.TextField()
+    star = models.FloatField()
+    coordinate = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+	    return self.title
+
+    class Meta:
+	    verbose_name_plural = 'Place'
+    
+	
+	
