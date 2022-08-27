54bb197691f7b0470823b8942c3f088a873942f5
fernanduandrade <onanduandrade@gmail.com>
2022-08-27 16:51:32 -0300

    add: adicionado pesquisa pelo id da categoria

--- app/core/views.py
+++ app/core/views.py
@@ -1,13 +1,15 @@
 from .models import Place, Category
 from .serializers import PlaceSerializer, CategorySerializer
 from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'category']
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['title']
+    filterset_fields = ['category']
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
