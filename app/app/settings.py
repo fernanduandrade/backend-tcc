deccf4885cac0cd5e1ea53c563ac6574bf8629f6
fernanduandrade <onanduandrade@gmail.com>
2022-08-31 01:18:30 -0300

    fix: corrigido porta

--- app/app/settings.py
+++ app/app/settings.py
@@ -103,7 +103,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': '5432',
     }
 }
 
