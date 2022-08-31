c48a718d2d9fadec88a0d98ce5cc9308a8dcb40c
fernanduandrade <onanduandrade@gmail.com>
2022-08-31 07:20:32 -0300

    fix: corrigido path

--- app/core/apps.py
+++ app/core/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
+import os
+from django.conf import settings
 
 
-class CoreConfig(AppConfig):
+class TranslatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    path = os.path.join(settings.BASE_DIR, 'core')
